b08d57294566ab2d4c5696864ec60faf80d36d32
Yury Zharikov <zharikovyuri@gmail.com>
2022-08-10 15:14:03 +0300

    it works
    '

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -8,5 +8,6 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('group_list.html/', views.group_list, name='group_list'),
-    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
+    path('group/<slug:slug>/', views.group_posts, name='group'),
 ]
+#posts:group_list
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
+    path('group/<slug:slug>/', include('posts.urls', namespace='posts')),
 ]
