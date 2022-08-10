a15a4099652de52313dad200023eb3a3edb1f436
Yury Zharikov <zharikovyuri@gmail.com>
2022-08-10 13:49:24 +0300

    almost done

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,11 +4,32 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.CharField(max_length=50)
+    description = models.TextField()
+
+    
+def __str__(self):
+    return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='group'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='posts'
     )
+
+
+# Укажите в модели метод __str__, 
+# чтобы при печати объекта модели Group выводилось поле title
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.urls import path
-from . import views
+from django.shortcuts import render, get_object_or_404
+#from django.urls import path
+#from . import views
+from .models import Post, Group
+
 
 
 # ice_cream/views.py
@@ -9,18 +11,36 @@ from django.http import HttpResponse
 
 # Главная страница
 def index(request):
-    template = 'posts/index.html'
-    text = 'Это главная страница проекта Yatube'
+    posts = Post.objects.all()
+    # другие способы получить посты (а как делать пагинацию?)
+    # posts = Post.objects.order_by('-pub_date')[:10]
+    # Post.objects.get(id=1)
+    # Post.objects.filter(pub_date__year=1854 
+    # Post.objects.filter(text__startswith='Писать не хочется')
     context = {
-        'text': text,
+        'posts': posts,
     }
-    return render(request, template)
+    return render(request, 'posts/index.html', context)
 
 # Страница с информацией об одном сорте мороженого;
 # view-функция принимает параметр pk из path()
 
-def group_posts(request, pk):
-    return HttpResponse(f'This is a post №{pk}')
+def group_posts(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'group': group,
+    }
+    return render(request, 'posts/group_list.html', context)
 
 # Create your views here.
 
