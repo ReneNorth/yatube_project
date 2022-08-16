417d5c6fe6da7645a3331cf9b88c4ef3fa68898c
ReneNorth <104070976+ReneNorth@users.noreply.github.com>
2022-08-16 10:37:35 +0300

    Update urls.py

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -20,5 +20,4 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
-    path('group/<slug:slug>/', include('posts.urls', namespace='posts')),
 ]
